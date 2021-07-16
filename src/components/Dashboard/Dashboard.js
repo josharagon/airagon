0a2dcc4b77675f94d2454906f05d93acc8ced653
Josh Aragon <josharagon99@gmail.com>
2021-07-15 23:24:07 -0600

    add signed in user

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,21 @@
 import "./Dashboard.css";
+import down from "../../img/down.png";
 
 const Dashboard = () => {
-  return <section className="dashboard"></section>;
+  return (
+    <section className="dashboard">
+      <div className="signed-in-user">
+        <h2>Good Morning, Josh</h2>
+        <div className="drop-down">
+          <img className="down" src={down}></img>
+          <div>
+            <p>joshcooks</p>
+            <p>#0001</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
