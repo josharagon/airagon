902585cc9c8d94c6f4d436ffc4b4d27fff5280e8
Josh Aragon <josharagon99@gmail.com>
2021-07-16 01:06:53 -0600

    Add checkout card

---
+++ src/components/CheckoutCard/CheckoutCard.js
@@ -0,0 +1,16 @@
+import "./CheckoutCard.css";
+
+const CheckoutCard = () => {
+  return (
+    <article>
+      <h1>img</h1>
+      <div className="details">
+        <p>product name</p>
+        <p>store name</p>
+        <p>cost on date</p>
+      </div>
+    </article>
+  );
+};
+
+export default CheckoutCard;
