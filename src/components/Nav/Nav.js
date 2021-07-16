1d4cf7bc5e80f3a916918b8f9a2fd8a34fc7d5ab
Josh Aragon <josharagon99@gmail.com>
2021-07-15 21:53:12 -0600

    Add all nav bar links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,72 @@
 import "./Nav.css";
 import logo from "../../img/aragonaio1.png";
+import stat from "../../img/statbar.png";
+import folder from "../../img/folder.png";
+import profile from "../../img/profile.png";
+import proxy from "../../img/proxy.png";
+import settings from "../../img/settings.png";
+
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="side-nav">
       <img className="logo" src={logo}></img>
-      <NavLink exact to={"/"} activeStyle={{ color: "white" }}>
-        Dashboard
-      </NavLink>
-      <NavLink exact to={"/profiles"} activeStyle={{ color: "white" }}>
-        Profiles
-      </NavLink>
-      <NavLink exact to={"/about"} activeStyle={{ color: "white" }}>
-        Proxies
-      </NavLink>
+      <div className="category-holder">
+        <p>General</p>
+        <NavLink exact to={"/"} activeStyle={{ color: "white" }}>
+          <div className="middle">
+            <img className="icon-stat" src={stat}></img>
+            <p className="button-text">Dashboard</p>
+          </div>
+        </NavLink>
+      </div>
+      <div className="category-holder">
+        <p>Setup</p>
+        <NavLink
+          exact
+          to={"/Tasks"}
+          activeStyle={{ color: "white" }}
+          className="multi-button"
+        >
+          <div className="middle">
+            <img className="icon" src={folder}></img>
+            <p className="button-text">Tasks</p>
+          </div>
+        </NavLink>
+
+        <NavLink
+          exact
+          to={"/profiles"}
+          activeStyle={{ color: "white" }}
+          className="multi-button"
+        >
+          <div className="middle">
+            <img className="icon-profile" src={profile}></img>
+            <p className="button-text">Profiles</p>
+          </div>
+        </NavLink>
+        <NavLink
+          exact
+          to={"/Proxies"}
+          activeStyle={{ color: "white" }}
+          className="multi-button"
+        >
+          <div className="middle">
+            <img className="icon-profile" src={proxy}></img>
+            <p className="button-text">Proxies</p>
+          </div>
+        </NavLink>
+      </div>
+      <div className="category-holder">
+        <p>Settings</p>
+        <NavLink exact to={"/settings"} activeStyle={{ color: "white" }}>
+          <div className="middle">
+            <img className="icon-settings" src={settings}></img>
+            <p className="button-text">Settings</p>
+          </div>
+        </NavLink>
+      </div>
     </nav>
   );
 };
