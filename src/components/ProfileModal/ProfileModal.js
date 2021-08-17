b7056205258ab0beea053a21bb13add73b9fd289
Josh Aragon <josharagon99@gmail.com>
2021-08-17 01:54:53 -0600

    Add profile modals final layout

--- src/components/ProfileModal/ProfileModal.js
+++ src/components/ProfileModal/ProfileModal.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import Modal from "react-modal";
 import "./ProfileModal.css";
 
+const customStyles = {
+  content: {
+    background: "#1a1c21",
+  },
+};
+
 Modal.setAppElement("#root");
 
 const ProfileModal = () => {
@@ -20,62 +26,78 @@ const ProfileModal = () => {
     <div>
       <button onClick={openModal}>Open Modal</button>
       <Modal
+        style={customStyles}
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="New Profile Modal"
       >
-        <div className="new-profile-header">
-          <h1>add new profile</h1>
-          <button onClick={closeModal} className="close-modal">
-            &#10006;
-          </button>
-        </div>
-        <div className="address-information">
-          <div className="shipping">
-            <h4>Shipping Info</h4>
-            <div className="names">
-              <input placeholder="First Name" />
-              <input placeholder="Last Name" />
+        <div className="modal-content">
+          <div className="new-profile-header">
+            <h1>add new profile</h1>
+            <button onClick={closeModal} className="close-modal">
+              &#10006;
+            </button>
+          </div>
+          <div className="modal-info">
+            <div className="address-information">
+              <div className="shipping">
+                <h4 className="new-profile-header">shipping Info</h4>
+                <div className="names">
+                  <input placeholder="First Name" />
+                  <input placeholder="Last Name" />
+                </div>
+                <input placeholder="Email" />
+                <input placeholder="Phone" />
+                <input placeholder="Street Address" />
+                <div>
+                  <input placeholder="City" />
+                  <input placeholder="State" />
+                </div>
+                <div>
+                  <input placeholder="Zip Code" />
+                  <input placeholder="Country" />
+                </div>
+              </div>
+              <div className="card">
+                <h4 className="new-profile-header">card information</h4>
+                <input placeholder="Card Number"></input>
+                <div>
+                  <input placeholder="Card Month"></input>
+                  <input placeholder="Card Year"></input>
+                </div>
+                <input placeholder="CVV"></input>
+              </div>
             </div>
-            <input placeholder="Email" />
-            <input placeholder="Phone" />
-            <input placeholder="Street Address" />
-            <div>
-              <input placeholder="City" />
-              <input placeholder="State" />
-            </div>
-            <div>
-              <input placeholder="Zip Code" />
-              <input placeholder="Country" />
+            <div className="card-information">
+              <div>
+                <div className="shipping">
+                  <h4 className="new-profile-header">billing</h4>
+                  <div className="names">
+                    <input placeholder="First Name" />
+                    <input placeholder="Last Name" />
+                  </div>
+                  <input placeholder="Email" />
+                  <input placeholder="Phone" />
+                  <input placeholder="Street Address" />
+                  <div>
+                    <input placeholder="City" />
+                    <input placeholder="State" />
+                  </div>
+                  <div>
+                    <input placeholder="Zip Code" />
+                    <input placeholder="Country" />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <h4 className="new-profile-header">profile</h4>
+                <input placeholder="Profile Name"></input>
+                <button>Save</button>
+                <button>Clear</button>
+              </div>
             </div>
           </div>
-          <div className="shipping">
-            <h4>billing</h4>
-            <div className="names">
-              <input placeholder="First Name" />
-              <input placeholder="Last Name" />
-            </div>
-            <input placeholder="Email" />
-            <input placeholder="Phone" />
-            <input placeholder="Street Address" />
-            <div>
-              <input placeholder="City" />
-              <input placeholder="State" />
-            </div>
-            <div>
-              <input placeholder="Zip Code" />
-              <input placeholder="Country" />
-            </div>
-          </div>
         </div>
-
-        <h4>Card Information</h4>
-        <input placeholder="Card Number"></input>
-        <div>
-          <input placeholder="Card Month"></input>
-          <input placeholder="Card Year"></input>
-        </div>
-        <input placeholder="CVV"></input>
       </Modal>
     </div>
   );
